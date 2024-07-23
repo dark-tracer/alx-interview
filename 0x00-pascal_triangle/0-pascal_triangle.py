1fda278714ba8859121c9baa675be460cd2fe8fd
dark-tracer <bernieamponsah2@gmail.com>
2024-07-23 19:27:33 +0000

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""Pascal triangle"""
+"""
+0-pascal_triangle
+"""
+
 
 def pascal_triangle(n):
     """
-    Generates Pascal's triangle up to the given n value.
-
-    Args:
-        n (int): The number of rows to generate in Pascal's triangle.
-
-    Returns:
-        List[List[int]]: A list of lists representing Pascal's triangle.
-
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
     """
+    k = []
     if n <= 0:
-        # Return an empty list if n <= 0
-        return []
-
-    triangle = [[1]]  # Initialize with the first row
-
+        return k
+    k = [[1]]
     for i in range(1, n):
-        row = [1]  # First element of each row is always 1
-        prev_row = triangle[i-1]
-
-        for j in range(1, i):
-            # Compute each element in the current row
-            element = prev_row[j-1] + prev_row[j]
-            row.append(element)
-
-        row.append(1)  # Last element of each row is always 1
-        triangle.append(row)
-
-    return triangle
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
